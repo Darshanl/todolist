04dc02240a39fcdcf7798fc435d5823237eaddd3
Darshanl <darshan.lakade157@gmail.com>
2019-01-26 11:14:45 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/todolist/index/precache-manifest.9fa090f828c23948a44507aa8380da78.js"
+  "/todolist/precache-manifest.0fc4bf25a0e3c06b0b1f4795a895e19d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/todolist/index/index.html", {
+workbox.routing.registerNavigationRoute("/todolist/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
