8c0aa95b37bd9b07b1d5411942bec267b0b5382e
Darshanl <darshan.lakade157@gmail.com>
2019-03-13 13:56:40 +0530

    modifyig files and adding icon

--- src/component/AddToList.js
+++ src/component/AddToList.js
@@ -6,13 +6,13 @@ const AddToList = ({addChange, textChange, keyPressed, searchField}) => {
         <div className="pa2">
             <input type='text' onChange={textChange} onKeyPress={keyPressed}
             name='searchField'
-            className="pa1 b--none bg-light-gray"
+            className="inp pa1 b--none bg-light-gray inputBG"
             value={searchField}
             placeholder="Add.."
             />
             <button onClick = {addChange}
-            className="btn pa1 bw1 b--none bg-light-red">
-                Add To List
+            className="btn pa1 bw1 bg-light-red inputbg">
+                <i class="fa fa-plus-square fa-1x" aria-hidden="true"></i>
             </button>
         </div>
     );
--- src/component/ToDosItem.js
+++ src/component/ToDosItem.js
@@ -23,7 +23,7 @@ const ToDosItem = ({title,chChange,done,user,delTodo}) => {
                 {title}
                 <button className="delete b--none"
                 onClick={delTodo.bind(this, id)}>
-                Delete</button>
+                <i class="fa fa-trash" aria-hidden="true"></i></button>
             </p>
         </div>
     );
